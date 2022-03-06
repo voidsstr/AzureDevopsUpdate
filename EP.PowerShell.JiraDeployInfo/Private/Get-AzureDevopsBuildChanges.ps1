9226858dba5c94aa7128f1fa23cae7ec13ecab62
Mike Perry <perrymb@gmail.com>
2022-03-06 18:58:02 -0500

    upda

--- EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsBuildChanges.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsBuildChanges.ps1
@@ -13,7 +13,7 @@ function Get-AzureDevopsBuildChanges {
     $response = Invoke-RestMethod -Uri $AzureChangeUrl -Headers @{Authorization = "Basic $token" } -Method Get
     Write-Verbose("Azure change response:" + ($response | ConvertTo-Json -Depth 100))
 
-    Write-Verbose("Build changes (artificatDatda.data)" + $response.fps.dataProviders.data.'ms.vss-traceability-web.traceability-run-changes-data-provider'.artifactsData.data)
+    Write-Verbose("Build changes (artificatDatda.data)" + $response.fps.dataProviders.data.'ms.vss-traceability-web.traceability-run-changes-data-provider'.artifactsData.data[0])
 
     $build_changes = $response.fps.dataProviders.data.'ms.vss-traceability-web.traceability-run-changes-data-provider'.artifactsData.data | ForEach-Object { $_.message }
     Write-Verbose("[Build Changes] " + $build_changes)
