16f6d71f82bb896090dbba2efc5283dca1dd455a
Mike Perry <perrymb@gmail.com>
2022-03-06 18:12:20 -0500

    updated

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -1,2 +1,5 @@
 Import-Module (Join-Path "." "EP.PowerShell.JiraDeployInfo") -Force
+
+Write-Output ("Forced reloading of functions...now running UpdateAzureDeploymentInformation")
+
 Update-AzureDeploymentInformation -State $Env:JIRA_STATE -EnvironmentType $Env:JIRA_ENVIRONMENT -JiraDomain $Env:JIRA_DOMAIN
