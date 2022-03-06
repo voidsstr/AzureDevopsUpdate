102141eb2d3a91051d030f3e4b5b12b8f641ccda
Mike Perry <perrymb@gmail.com>
2022-03-06 18:59:42 -0500

    upda

--- EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
@@ -2,8 +2,11 @@ function Find-JiraIDs {
     param (
         $message
     )
+
+    Write-Verbose ("Message being parsed for ids:" + $message)
+
     $pattern = '\w{2,10}-\d{1,4}'
     $values = [regex]::Matches($message, $pattern) | Select-Object value 
-    Write-Debug ("[JIRA IDs] " + $values)
+    Write-Verbose ("[JIRA IDs] " + $values)
     $values
 }
