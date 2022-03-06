cd5ac2d64a75fc978396f02ce07c6923ed6b607d
Mike Perry <perrymb@gmail.com>
2022-03-06 18:09:06 -0500

    updated

--- EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsBuildChanges.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsBuildChanges.ps1
@@ -7,10 +7,12 @@ function Get-AzureDevopsBuildChanges {
         [string] $AzureChangeUrl 
     )
     
+    Write-Output($AzureChangeUrl)
+
     $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($SystemAccessToken)"))
     $response = Invoke-RestMethod -Uri $AzureChangeUrl -Headers @{Authorization = "Basic $token" } -Method Get
     Write-Debug $response
     $build_changes = $response.fps.dataProviders.data.'ms.vss-traceability-web.traceability-run-changes-data-provider'.artifactsData.data | ForEach-Object { $_.message }
-    Write-Debug ("[Build Changes] " + $build_changes)
+    Write-Output ("[Build Changes] " + $build_changes)
     $build_changes
 }
