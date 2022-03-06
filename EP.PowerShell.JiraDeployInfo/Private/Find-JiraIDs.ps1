c36e82e539bad4efaea35f4edad75c5b2e701a7b
mike <mperry@glscrip.com>
2022-03-06 18:21:06 -0500

    asdf

--- EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
@@ -4,6 +4,6 @@ function Find-JiraIDs {
     )
     $pattern = '\w{2,10}-\d{1,4}'
     $values = [regex]::Matches($message, $pattern) | Select-Object value 
-    Write-Output ("[JIRA IDs] " + $values)
+    Write-Debug ("[JIRA IDs] " + $values)
     $values
 }
